61d898e57b728b42b1fb49329263143fdd414f1e
Gopinath Vasan <vasangopinath11@gmail.com>
2024-03-31 21:32:56 +0530

    update url

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Replace with your frontend URL
+    allow_origins=["https://austinpartnership.in"],  # Replace with your frontend URL
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE"],
     allow_headers=["*"],
