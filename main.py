5fe93fb85ffc665b15bec7975b1718687b7641f3
Gopinath Vasan <vasangopinath11@gmail.com>
2024-08-01 00:38:04 +0530

    New fie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import auth,users,forgotpassword,otp,dashboard
+from routers import auth, users, forgotpassword, otp,dashboard
 
 
 # /clientlogin
@@ -9,8 +9,8 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://www.austinpartnership.in"],
-    # allow_origins=["http://localhost:3000"],  # Replace with your frontend URL
+    # allow_origins=["https://www.austinpartnership.in"],
+    allow_origins=["http://localhost:3000"],  # Replace with your frontend URL
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"]
