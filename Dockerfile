852319a8eeed7b2b9970ee048ba406fcb26763d4
Gopinath Vasan <vasangopinath11@gmail.com>
2024-08-09 23:06:10 +0530

    docker sertup1

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,11 @@
-# Use an official Python runtime as a parent image
+# Use the official Python image from the Docker Hub
 FROM python:3.9-slim
 
 # Set the working directory in the container
-WORKDIR /main
+WORKDIR /app
 
 # Copy the current directory contents into the container at /app
-COPY . /main
+COPY . /app
 
 # Install any needed packages specified in requirements.txt
 RUN pip install --no-cache-dir -r requirements.txt
@@ -13,5 +13,5 @@ RUN pip install --no-cache-dir -r requirements.txt
 # Make port 80 available to the world outside this container
 EXPOSE 80
 
-# Run app.py when the container launches
-CMD ["python", "main.py"]
+# Run the FastAPI app using uvicorn
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
--- main.py
+++ main.py
@@ -1,24 +1,32 @@
+import logging
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from routers import auth, users, forgotpassword, otp,dashboard
+from routers import auth, users, forgotpassword, otp, dashboard
 
-
-# /clientlogin
+# Configure logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=["https://www.austinpartnership.in"],
     allow_origins=["http://localhost:3000"],  # Replace with your frontend URL
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allow_headers=["*"]
 )
 
-
 app.include_router(auth.router)
 app.include_router(users.router)
 app.include_router(forgotpassword.router)
 app.include_router(otp.router)
-app.include_router(dashboard.router)    
+app.include_router(dashboard.router)
+
+@app.on_event("startup")
+async def startup_event():
+    logger.info("Starting up...")
+
+@app.on_event("shutdown")
+async def shutdown_event():
+    logger.info("Shutting down...")
